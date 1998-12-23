6efa6b41ff77407c7426363f16a4c15d36d5b7cf
set <>
1998-12-23 01:29:01 +0000

    Changed the release number.

--- tvision/djgpp/compress.bat
+++ tvision/djgpp/compress.bat
@@ -1,6 +1,6 @@
 cd ..\..
-rm tv100s.zip tv100b.zip
-zip -r9 tv100s tvision -x *.o *.gdt *.bkp *.dst *.a "*CVS/*" "*cvs/*"
-zip -9 tv100b @tvision/djgpp/distlist
+rm tv101s.zip tv101b.zip
+zip -r9 tv101s tvision -x *.o *.gdt *.bkp *.dst *.a "*CVS/*" "*cvs/*"
+zip -9 tv101b @tvision/djgpp/distlist
 cd tvision\djgpp
 
