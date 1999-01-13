af3faddc04d45c3bd1181e80a9eb72474fd23600
set <>
1999-01-13 00:57:08 +0000

    Typo fixed, missing R in FilterValidator.

--- tvision/stream/srangeva.cc
+++ tvision/stream/srangeva.cc
@@ -1,7 +1,7 @@
 #define Uses_TStreamableClass
 #define Uses_TRangeValidator
 #include <tv.h>
-__link(FilterValidator)
+__link(RFilterValidator)
 
 TStreamableClass RRangeValidator( TRangeValidator::name,
                          TRangeValidator::build,
