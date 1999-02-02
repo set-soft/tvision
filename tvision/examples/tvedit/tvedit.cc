4f50a6745507b85ed7de2e8c8486ccda80685862
Salvador E. Tropea <salvador@inti.gob.ar>
1999-02-02 01:56:57 +0000

    * Added as a test for TEditorApp.

---
+++ tvision/examples/tvedit/tvedit.cc
@@ -0,0 +1,27 @@
+/*****************************************************************************
+
+  Hi! here SET writing ;-)
+  That's a pretty silly example, but I include it more as a test. The real
+code for this example is inside the library and is called tvedit?.cc.
+Basically it defines a basic text editor applition. You can create your own
+text editor inheriting from TEditorApp and overriding the desired members.
+  Robert included it in the library in the past and I think is good idea
+because you can do an application based on TEditorApp in a few minutes.
+
+*****************************************************************************/
+
+#define Uses_TApplication
+#define Uses_TEditorApp
+#include <tv.h>
+#include <tvedit.h>
+
+int main()
+{
+ TEditorApp *myApp=new TEditorApp();
+ if (myApp)
+   {
+    myApp->run();
+    delete myApp;
+   }
+ return 0;
+}
