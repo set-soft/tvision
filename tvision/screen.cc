7c1a420a0f876c16be0323fb64e75f5e5256ddcb
root <>
1999-05-16 22:09:18 +0000

    * Added: My DOS TSR to enhance the keyboard to the distribution. * Added: A .pif example showing how to prevent using the program in a window and recover some keys like Alt+Space. * Moved the Linux code from screen.cc to classes/linux/tscreen.cc. * Added: The ability to use VCSAs even if they are write only. * Removed the tcget/setattr calls from TScreen because that's done by ncurses. * Added: A variable to fine tune the video driver called TurboVision_screenOptions, by now only have meaning under Linux and is used to tell the TScreen class to not use VCSAs if they are write only (because then suspend/resume doesn't work properly). * Modified: Now Alt+key doesn't have any ASCII value associated so if you press Alt+8 the program won't display 8 if there is no action for Alt+8. * Added: Documentation in the readmes about the limitations and particularities of each platform. For example: what doesn't work when using telnet, how can you paste using gpm, etc.

--- tvision/screen.cc
+++ tvision/screen.cc
@@ -1,5 +1,7 @@
 /* Copyright (C) 1996-1998 Robert H”hne, see COPYING.RH for details */
 /* This file is part of RHIDE. */
+#ifdef __DJGPP__
+// SET: Moved the Linux code to tscreen.cc
 #define Uses_TScreen
 #include <tv.h>
 #include <stdlib.h>
@@ -17,58 +19,30 @@ void rh_RestoreVideo();
 void set_SaveVideo(int,int);
 void set_RestoreVideo();
 
-#ifndef __DJGPP__
-static ushort *user_buffer=0;
-static int user_buffer_size;
-static int user_cursor_x,user_cursor_y;
-extern int vcs_fd;
-#endif
-
 void SaveScreen()
 {
-#ifdef __DJGPP__
   if (screen_saving == MODE_SET)
     set_SaveVideo(TDisplay::getCols(),TDisplay::getRows());
   else
   if (screen_saving == MODE_RH)
     rh_SaveVideo(TDisplay::getCols(),TDisplay::getRows());
-#else
-  if (vcs_fd >= 0)
-  {
-    user_buffer_size = TScreen::getCols()*TScreen::getRows();
-    user_buffer = (ushort *)realloc(user_buffer,user_buffer_size*sizeof(ushort));
-    TScreen::getCharacter(0,user_buffer,user_buffer_size);
-    TScreen::GetCursor(user_cursor_x,user_cursor_y);
-  }
-#endif
 }
 
 // SET: Added to release the memory allocated here
 void SaveScreenReleaseMemory(void)
 {
-#ifdef __DJGPP__
   if (screen_saving == MODE_RH)
      rh_SaveVideoReleaseMemory();
-#else
-  free(user_buffer);
-#endif
 }
 
 void RestoreScreen()
 {
-#ifdef __DJGPP__
   if (screen_saving == MODE_SET)
     set_RestoreVideo();
   else
   if (screen_saving == MODE_RH)
     rh_RestoreVideo();
-#else
-  if (vcs_fd >= 0)
-  {
-    TScreen::setCharacter(0,user_buffer,user_buffer_size);
-    TScreen::SetCursor(user_cursor_x,user_cursor_y);
-  }
-#endif
 }
+#endif
 
 
