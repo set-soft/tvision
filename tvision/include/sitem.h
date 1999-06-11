76f616031816f6ff33a2320274f8b11192ed60aa
set <>
1999-06-11 23:55:51 +0000

    * Fixed: calls to delete passing void *.

--- tvision/include/sitem.h
+++ tvision/include/sitem.h
@@ -21,7 +21,7 @@ public:
 
     TSItem( const char *aValue, TSItem *aNext )
         { value = newStr(aValue); next = aNext; }
-    ~TSItem() { delete (void *)value; }
+    ~TSItem() { delete[] value; }
 
     const char *value;
     TSItem *next;
