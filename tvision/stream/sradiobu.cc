f0b69800cd7c4015c6f9aa1a61a9532fba8850fb
Salvador E. Tropea <salvador@inti.gob.ar>
1999-08-11 01:44:47 +0000

    * Fixed: TCheckBoxes & TRadioButtons problem with streams

--- tvision/stream/sradiobu.cc
+++ tvision/stream/sradiobu.cc
@@ -2,6 +2,7 @@
 #define Uses_TStreamableClass
 #include <tv.h>
 
+__link( RCluster )
 TStreamableClass RRadioButtons( TRadioButtons::name,
                                 TRadioButtons::build,
                                 __DELTA(TRadioButtons)
