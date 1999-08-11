252b958fd9f243788ee216c9a649f5c1f8359145
set <>
1999-08-11 01:44:47 +0000

    * Fixed: TCheckBoxes & TRadioButtons problem with streams

--- tvision/stream/scheckbo.cc
+++ tvision/stream/scheckbo.cc
@@ -2,6 +2,7 @@
 #define Uses_TStreamableClass
 #include <tv.h>
 
+__link( RCluster )
 TStreamableClass RCheckBoxes( TCheckBoxes::name,
                               TCheckBoxes::build,
                               __DELTA(TCheckBoxes)
