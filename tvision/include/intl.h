0d8ef4092240f4f8da67473b42f1edfec1411a06
root <>
1999-09-08 02:11:45 +0000

    * Unified the international support headers, now the use configtv.h to determine if the intl support is used or not.

--- tvision/include/intl.h
+++ tvision/include/intl.h
@@ -1,5 +1,14 @@
 /* Copyright (C) 1996-1998 Robert H”hne, see COPYING.RH for details */
 /* This file is part of RHIDE. */
+
+#ifdef FORCE_INTL_SUPPORT
+#define HAVE_INTL_SUPPORT
+#else
+#include <configtv.h>
+#endif
+
+#ifdef HAVE_INTL_SUPPORT
+
 #include <libintl.h>
 #ifdef __cplusplus
 extern "C" {
@@ -50,3 +59,49 @@ char *gettext__(const char *);
 #if !defined( __ )
 #define __(msg) GETTEXT_NOOP(msg)
 #endif
+
+#else  /* HAVE_INTL_SUPPORT */
+
+/***************************** NO intl support ***************************/
+#ifndef __RH_INTL_H__
+#define __RH_INTL_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#if !defined( GETTEXT )
+#define GETTEXT gettext__
+#endif
+
+#if !defined( gettext_noop )
+#define gettext_noop(msg) (msg)
+#endif
+
+#if !defined( GETTEXT_NOOP )
+#define GETTEXT_NOOP gettext_noop
+#endif
+
+#if !defined( TEXTDOMAIN )
+#define TEXTDOMAIN 
+#endif
+
+#if !defined( BINDTEXTDOMAIN )
+#define BINDTEXTDOMAIN
+#endif
+
+#if !defined( _ )
+#define _(msg) (msg)
+#endif
+
+#if !defined( __ )
+#define __(msg) GETTEXT_NOOP(msg)
+#endif
+
+#endif
+
+#endif /* else HAVE_INTL_SUPPORT */
