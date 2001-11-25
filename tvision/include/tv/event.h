9ae543bc345da8b69a5d71899753d5715bf9fdb7
root <>
2001-11-25 00:10:37 +0000

    * New THWMouse class.

--- tvision/include/tv/event.h
+++ tvision/include/tv/event.h
@@ -4,10 +4,8 @@
  *      Copyright (c) 1994 by Borland International
  *      All Rights Reserved.
  *
-
-Modified by Robert H”hne to be used for RHIDE.
-
- *
+ * Modified by Robert H”hne to be used for RHIDE.
+ * SET: Hardware abstaction with ideas from JASC.
  *
  */
 
@@ -16,113 +14,150 @@ Modified by Robert H”hne to be used for RHIDE.
 
 struct MouseEventType
 {
-    uchar buttons;
-    Boolean doubleClick;
-    TPoint where;
+ uchar buttons;
+ Boolean doubleClick;
+ TPoint where;
 };
 
+// This class is the base hardware interface with the mouse and shouldn't
+// be used directly. You should use TMouse instead which is derived from
+// it. That's why most members are protected.
 class THWMouse
 {
-
 protected:
+ THWMouse();
+ THWMouse(const THWMouse&) {};
+ virtual ~THWMouse();
 
-    THWMouse();
-    THWMouse( const THWMouse& ) {};
-    virtual ~THWMouse();
+ static void show();
+ static void hide();
 
-    static void show();
-    static void hide();
+ static void (*setRange)(ushort, ushort);
+ static void getEvent(MouseEventType &me);
+ // This function could fail according to the hardware.
+ // In TV 2.0 it just doesn't exist when you compile for 32 bits.
+ static void (*registerHandler)(unsigned, void (*)());
 
-    static void setRange( ushort, ushort );
-    static void getEvent( MouseEventType& );
-    static void registerHandler( unsigned, void (*)() );
-    static Boolean present();
+ static void suspend();
+ static void resume();
 
-    static void suspend();
-    static void resume();
-    static void inhibit();
+ // Inline members:
+ // Disable mouse
+ static void inhibit();
+ // Is mouse installed?
+ static Boolean present();
+
+ // SET: To override just part of the behavior:
+ static void (*Show)();
+ static void (*Hide)();
+ static void (*Suspend)();
+ static void (*Resume)();
+ static void (*GetEvent)(MouseEventType &me);
+
+ // SET: This is optional, is only needed if the harware uses forceEvent.
+ static void (*drawMouse)(int x, int y);
+
+ // SET: Default behaviors
+ static void defaultShow();
+ static void defaultHide();
+ static void defaultSetRange(ushort, ushort);
+ static void defaultGetEvent(MouseEventType&);
+ static void defaultRegisterHandler(unsigned, void (*)());
+ static void defaultSuspend();
+ static void defaultResume();
+ static void defaultDrawMouse(int x, int y);
 
 public:
-    static void forceEvent(int x, int y, int buttons);
+ // SET: Used to externally force a mouse event.
+ // This is only used internally.
+ static void forceEvent(int x, int y, int buttons);
 
 protected:
+ // This indicates how many buttons have the mouse. Is also used to determine
+ // if the mouse is present, a value of 0 is mouse not available. See the
+ // present() member.
+ static uchar buttonCount;
+ // SET: Just to avoid redundant calls
+ static char  visible;
+ // SET: Data used to force an event externally
+ static MouseEventType forcedME;
+ static char forced;
 
-    static uchar buttonCount;
-
-private:
-
-    static Boolean handlerInstalled;
-    static Boolean noMouse;
-
+ // SET: Moved to the protected section
+ static Boolean handlerInstalled;
+ static Boolean noMouse;
 };
 
 inline Boolean THWMouse::present()
 {
-    return Boolean( buttonCount != 0 );
+ return Boolean(buttonCount!=0);
 }
 
 inline void THWMouse::inhibit()
 {
-    noMouse = True;
+ noMouse=True;
 }
 
+// This class exposses the mouse interface.
 class TMouse : public THWMouse
 {
-
 public:
+ TMouse();
+ ~TMouse();
 
-    TMouse();
-    ~TMouse();
+ static void show();
+ static void hide();
 
-    static void show();
-    static void hide();
+ static void suspend();
+ static void resume();
 
-    static void setRange( ushort, ushort );
+ static void setRange( ushort, ushort );
 
-    static void getEvent( MouseEventType& );
-    static void registerHandler( unsigned, void (*)() );
-    static Boolean present();
-
-    static void suspend() { THWMouse::suspend(); }
-    static void resume();
-
+ static void getEvent( MouseEventType& );
+ static void registerHandler( unsigned, void (*)() );
+ static Boolean present();
 };
 
 inline void TMouse::show()
 {
-    THWMouse::show();
-}
-
-inline void TMouse::resume()
-{
-  THWMouse::resume();
-//  show();
+ THWMouse::show();
 }
 
 inline void TMouse::hide()
 {
-    THWMouse::hide();
+ THWMouse::hide();
 }
 
-inline void TMouse::setRange( ushort rx, ushort ry )
+inline void TMouse::suspend()
 {
-    THWMouse::setRange( rx, ry );
+ THWMouse::suspend();
 }
 
-inline void TMouse::getEvent( MouseEventType& me )
+inline void TMouse::resume()
 {
-    THWMouse::getEvent( me );
+ THWMouse::resume();
 }
 
-inline void TMouse::registerHandler( unsigned mask, void (*func)() )
+inline void TMouse::setRange(ushort rx, ushort ry)
 {
-    THWMouse::registerHandler( mask, func );
+ THWMouse::setRange(rx,ry);
+}
+
+inline void TMouse::getEvent(MouseEventType& me)
+{
+ THWMouse::getEvent(me);
+}
+
+inline void TMouse::registerHandler(unsigned mask, void (*func)())
+{
+ THWMouse::registerHandler(mask,func);
 }
 
 inline Boolean TMouse::present()
 {
-    return THWMouse::present();
+ return THWMouse::present();
 }
+/****************************************************************************************/
 
 struct CharScanType
 {
