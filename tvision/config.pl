621cf470b31abf0d8ac430b9b136c10cebbcb28b
root <>
2002-01-03 02:13:35 +0000

    * Fixed: Win32 helper.

--- tvision/config.pl
+++ tvision/config.pl
@@ -137,7 +137,8 @@ if ($OS ne 'Win32')
    $MakeDefsRHIDE[3].=' ../../intl/dummy' if $UseDummyIntl;
    $ExtraModifyMakefiles{'vpath_src'}="../classes/win32 ../stream ../names ../classes .. ../djgpp\nvpath %.h ../djgpp";
    `cp djgpp/makefile win32/Makefile`;
-   ModifyMakefiles('win32/Makefile');
+   ModifyMakefiles('win32/Makefile','compat/compat.mak');
+   CreateRHIDEenvs('examples/rhide.env','win32/rhide.env');
   }
 # Help BC++ target
 `perl confignt.pl`;
