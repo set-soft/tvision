84a53a3da085c2a561e0c3524ba3920e9021c5ed
set <>
2002-05-16 21:22:35 +0000

    * Modified: [Linux] Added Scroll-Lock to the patch list, it freezies the application and some people thinks it hanged. To make it worst some idiots put the S-L key very close to Home just to insert a stupid key to make the machine sleep and hence is quite easy to press S-L instead of Home. So now TV remaps S-L to Home. From a report by David A. Capello <dacap@users.sourceforge.net>.

--- tvision/linux/gkeyli.cc
+++ tvision/linux/gkeyli.cc
@@ -629,6 +629,11 @@ typedef struct
 #define SCAN_TAB  15
 // As ^[ is usually Esc we must be sure it doesn't happend
 #define SCAN_LFBR 26
+// The Scroll lock key is evil for an interactive application
+// As in my keyboard that's above Home and some people wrongly
+// press it instead of Home I'm translating to Home.
+#define SCAN_SCRL 70
+#define SCAN_HOME 102
 
 change_entry changes[] = {
   { kblAltL,  SCAN_F1,   kblNormal, SCAN_F1,   0, 0},
@@ -681,6 +686,9 @@ change_entry changes[] = {
   { kblCtrl,  SCAN_BKSP, kblNormal, SCAN_BKSP, 0, 0},
   { kblCtrl,  SCAN_SPAC, kblNormal, SCAN_SPAC, 0, 0},
   { kblCtrl,  SCAN_LFBR, kblNormal, SCAN_LFBR, 0, 0},
+  { kblNormal,SCAN_SCRL, kblNormal, SCAN_HOME, 0, 0},
+  { kblAltL,  SCAN_SCRL, kblNormal, SCAN_HOME, 0, 0},
+  { kblAltR,  SCAN_SCRL, kblNormal, SCAN_HOME, 0, 0},
   { kblCtrl | kblShift,  SCAN_TAB, kblNormal, SCAN_TAB, 0, 0}
 };
 
