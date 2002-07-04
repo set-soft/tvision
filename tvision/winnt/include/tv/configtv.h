6c3a0e8f171470b08514fcc6fa56a607af7ba61a
set <>
2002-07-04 20:09:59 +0000

    * Modified: [BC++/MSVC] these compilers doesn't use the configuration mechanism instead a custom configtv.h is used. I unified it for both compilers. Untested.

--- tvision/winnt/include/tv/configtv.h
+++ tvision/winnt/include/tv/configtv.h
@@ -1,16 +1,15 @@
-/* Generated automatically by the configure script */
+/* Specially created for BC++ and MSVC */
 
-/* ncurses 4.2 or better have define_key */
-//#define HAVE_DEFINE_KEY
-
-/* The X11 keysyms are there */
-//#define HAVE_KEYSYMS
-
-/* International support with gettext */
-//#define HAVE_INTL_SUPPORT
+#if defined(_MSVC) || defined(__MSC_VER) || defined(_MSC_VER)
+ /* Microsoft C compiler */
+ #define TVComp_MSC
+#else
+ /* Borland C++ compiler */
+ #define TVOSf_NT
+ #define TVComp_BCPP
+#endif
 
 #define TVOS_Win32
-#define TVOSf_NT
-#define TVComp_BCPP
 #define TVCompf_
 #define TVCPU_x86
+
