9705ab22a4fd3948cee33357c0af673b17e5fbdc
Salvador E. Tropea <salvador@inti.gob.ar>
2002-08-12 16:00:19 +0000

    * Fixed: [Cygwin] The CLY_GetShellName() function to return the content of SHELL environment variable as in UNIX. Found using the "videomode" example while testing with Cygwin, now the "Shell" option works ok.

--- tvision/compat/getshell.c
+++ tvision/compat/getshell.c
@@ -1,18 +1,18 @@
 /*
- Copyright (C) 2001      Salvador E. Tropea
+ Copyright (C) 2001,2002      Salvador E. Tropea
  Covered by the GPL license.
 */
 #define Uses_stdlib
 #include <compatlayer.h>
 
-#if defined(TVOS_DOS) || defined(TVOS_Win32)
+#if (defined(TVOS_DOS) || defined(TVOS_Win32)) && !defined(TVCompf_Cygwin)
 char *CLY_GetShellName(void)
 {
  return getenv("COMSPEC");
 }
 #endif
 
-#ifdef TVOS_UNIX
+#if defined(TVOS_UNIX) || defined(TVCompf_Cygwin)
 char *CLY_GetShellName(void)
 {
  return getenv("SHELL");
