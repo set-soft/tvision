a6fd78862a40bd62f9916c43b27660b6bab4e444
Salvador E. Tropea <salvador@inti.gob.ar>
2002-08-26 13:35:29 +0000

    * Added: The test code I use for Linux/Console cyrillic support.

---
+++ tvision/examples/cyrillic/linuxkoi8/setkoi8.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# This script sets a russian configuration.
+# Is customized for my personal needs and is only an example.
+#
+# Load a KOI8-R font.
+#
+consolechars -f koi8-r.psf.gz
+#
+# Load a russian keyboard map.
+#
+loadkeys ru1.kmap.gz
---
+++ tvision/examples/cyrillic/linuxkoi8/test.cc
@@ -0,0 +1,49 @@
+#define Uses_TApplication
+#define Uses_TKeys
+#define Uses_TRect
+#define Uses_TMenuBar
+#define Uses_TSubMenu
+#define Uses_TMenuItem
+#define Uses_TStatusLine
+#define Uses_TStatusItem
+#define Uses_TStatusDef
+#define Uses_TDeskTop
+#include <tv.h>
+
+class TMyApp : public TApplication
+{
+public:
+ TMyApp();
+ static TMenuBar *initMenuBar(TRect r);
+};
+
+TMyApp::TMyApp() :
+    TProgInit(&TMyApp::initStatusLine,
+              &TMyApp::initMenuBar,
+              &TMyApp::initDeskTop
+             )
+{
+}
+
+TMenuBar *TMyApp::initMenuBar( TRect r )
+{
+ r.b.y=r.a.y+1;
+ return new TMenuBar( r,
+     //*new TSubMenu("~F~ile",kbAltF)+
+     *new TSubMenu("~ๆ~มสฬ",0)+ // KOI8 test
+         *new TMenuItem("~O~pen",200,kbF3,hcNoContext,"F3")+
+         *new TMenuItem("~N~ew", 200,kbF4,hcNoContext,"F4")+
+         newLine()+
+         *new TMenuItem("E~x~it",cmQuit,cmQuit,hcNoContext,"Alt-X")+
+     *new TSubMenu("~W~indow", kbAltW )+
+         *new TMenuItem("~N~ext",cmNext,kbF6,hcNoContext,"F6")+
+         *new TMenuItem("~Z~oom",cmZoom,kbF5,hcNoContext,"F5")
+     );
+}
+
+int main()
+{
+ TMyApp myApp;
+ myApp.run();
+ return 0;
+}
