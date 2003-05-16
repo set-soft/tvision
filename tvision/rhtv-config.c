024598415d2a80e6a18e2395d5251fcd794ceb23
Salvador E. Tropea <salvador@inti.gob.ar>
2003-05-16 12:38:18 +0000

    * Added: A small tool called "rhtv-config.exe" that can be used to determine the libraries needed to link with TV (statically and dynamically), the path for libraries and the path for includes. I'm quite sure it doesn't work perfectly but I was able to compile the tvedit example using: gcc -o tvedit.exe `rhtv-config.exe --include` tvedit.cc `rhtv-config.exe --dir-libs` `rhtv-config.exe --dlibs` * Modified: To ever include stdc++ as library in the worst case it will be listed twice.

---
+++ tvision/rhtv-config.c
@@ -0,0 +1,159 @@
+/**[txh]********************************************************************
+
+  Copyright (c) 2003 by Salvador Eduardo Tropea
+  Covered by the GPL license.
+
+  Description:
+  Small program to help configuring packages using Turbo Vision.
+  
+***************************************************************************/
+
+#define Uses_getcwd
+#define Uses_stdio
+#define Uses_string
+#define Uses_limits
+#define Uses_stdlib
+#include <compatlayer.h>
+
+static char *curdir=TVCONFIG_REF_DIR;
+static int   lcurdir;
+
+static
+void StdInc(void)
+{
+ puts(TVCONFIG_RHIDE_STDINC);
+}
+
+/*static
+char *FixRelative(const char *s)
+{
+ if (strncmp(s,"../../",6)==0)
+   {
+    char *ret=(char *)malloc(lcurdir+strlen(s)+1-5);
+    sprintf(ret,"%s%s",curdir,s+5);
+    return ret;
+   }
+ return strdup(s);
+}*/
+
+static
+void Include(void)
+{
+ char *copy=strdup(TVCONFIG_TVSRC);
+ char *s=strtok(copy," ");
+
+ while (s)
+   {
+    if (*s && *s!='.')
+      {
+       printf("-I%s ",s);
+      }
+    s=strtok(NULL," ");
+   }
+}
+
+static
+void SLibs(void)
+{
+ char *copy=strdup(TVCONFIG_RHIDE_OS_LIBS);
+ char *s=strtok(copy," ");
+
+ printf("-Wl,-dn -lrhtv -Wl,-dy ");
+
+ while (s)
+   {
+    if (*s && *s!='.')
+      {
+       printf("-l%s ",s);
+      }
+    s=strtok(NULL," ");
+   }
+}
+
+static
+void DLibs(void)
+{
+ char *copy=strdup(TVCONFIG_RHIDE_OS_LIBS);
+ char *s=strtok(copy," ");
+
+ printf("-lrhtv ");
+ if (strstr(TVCONFIG_RHIDE_OS_LIBS,"tvfintl"))
+    printf("-ltvfintl");
+}
+
+static
+void DirLibs(void)
+{
+ char *copy=strdup(TVCONFIG_TVOBJ);
+ char *s=strtok(copy," ");
+
+ while (s)
+   {
+    if (*s && *s!='.')
+      {
+       printf("-L%s ",s);
+      }
+    s=strtok(NULL," ");
+   }
+}
+
+static
+void Usage(void)
+{
+ fprintf(stderr,"Usage: rhtv-config.exe OPTION\n");
+ fprintf(stderr,"Available options: [Only one can be specified]\n");
+ fprintf(stderr,"\t--stdinc\n");
+ fprintf(stderr,"\t--include\n");
+ fprintf(stderr,"\t--slibs   [for linking static]\n");
+ fprintf(stderr,"\t--dlibs   [for linking dynamic]\n");
+ fprintf(stderr,"\t--dir-libs\n");
+}
+
+static
+void UnknowOp(const char *s)
+{
+ fprintf(stderr,"Unknown option: %s\n\n",s);
+ Usage();
+}
+
+static
+void SetUpCurDir(void)
+{
+ lcurdir=strlen(curdir);
+}
+
+int main(int argc, char *argv[])
+{
+ char *op;
+ if (argc!=2)
+   {
+    Usage();
+    return 1;
+   }
+ op=argv[1];
+ if (op[0]!='-' || op[1]!='-')
+   {
+    UnknowOp(op);
+    return 2;
+   }
+ SetUpCurDir();
+ op+=2;
+ if (strcmp(op,"stdinc")==0)
+    StdInc();
+ else if (strcmp(op,"include")==0)
+    Include();
+ else if (strcmp(op,"slibs")==0)
+    SLibs();
+ else if (strcmp(op,"dlibs")==0)
+    DLibs();
+ else if (strcmp(op,"dir-libs")==0)
+    DirLibs();
+ else
+   {
+    UnknowOp(op);
+    return 2;
+   }
+ printf("\n");
+ return 0;
+}
+
