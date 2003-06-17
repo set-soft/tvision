e97dcf491db228f430a0d64fb68189ad7334d391
Salvador E. Tropea <salvador@inti.gob.ar>
2003-06-17 12:43:33 +0000

    * Added: A script to generate the RPMs in an easy way.

---
+++ tvision/redhat/create-rpms.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copyright (C) 2003 by Salvador E. Tropea (SET),
+# see copyrigh file for details
+#
+# This file executes the steps needed to create the RPM files. It doesn't
+# check for errors and is really simple.
+# It temporally changes the ~/.rpmmacros file, but the file is restored
+# before exiting. If you stop the script you can recover the original file
+# using the move located at the end.
+# The file should be executed from the ../
+#
+./configure --just-spec
+redhat/create-dirs.sh
+mv ~/.rpmmacros redhat/.rpmmacros-backup~
+redhat/config-rpm.sh
+cd linux; ./compress --use-bzip2 --dir-version; cd ..
+mv ../rhtvision-`cat version.txt`.src.tar.bz2 ~/rpm/SOURCES/librhtv-`cat version.txt`.tar.bz2
+rpmbuild -bb redhat/librhtv-`cat version.txt`.spec
+mv redhat/.rpmmacros-backup~ ~/.rpmmacros
+
