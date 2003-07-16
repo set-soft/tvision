5ab3ef8735af3a9695ffdb197b15c54b6f848645
Salvador E. Tropea <salvador@inti.gob.ar>
2003-07-16 14:00:58 +0000

    * Modified: The RPM scripts to support an environment variable to indicate where to create the RPMs. I needed it when using S.F. Compiler Farm and Red Hat 7.3 which always failed when using a directory mounted with NFS.

--- tvision/redhat/config-rpm.sh
+++ tvision/redhat/config-rpm.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-echo "%_topdir       " $HOME/rpm > ~/.rpmmacros
-
+if [ "$SET_RPM_DIR" ]; then
+   SET_RPM_DIR="$SET_RPM_DIR";
+else
+   SET_RPM_DIR="$HOME";
+fi
+echo "%_topdir       " $SET_RPM_DIR/rpm > ~/.rpmmacros
--- tvision/redhat/create-rpms.sh
+++ tvision/redhat/create-rpms.sh
@@ -9,12 +9,19 @@
 # using the move located at the end.
 # The file should be executed from the ../
 #
+if [ "$SET_RPM_DIR" ]; then
+   SET_RPM_DIR="$SET_RPM_DIR";
+else
+   SET_RPM_DIR="$HOME";
+fi
 ./configure --just-spec
+chmod +x redhat/create-dirs.sh
+chmod +x redhat/config-rpm.sh
 redhat/create-dirs.sh
 mv ~/.rpmmacros redhat/.rpmmacros-backup~
 redhat/config-rpm.sh
 cd linux; ./compress --use-bzip2 --dir-version; cd ..
-mv ../rhtvision-`cat version.txt`.src.tar.bz2 ~/rpm/SOURCES/librhtv-`cat version.txt`.tar.bz2
+mv ../rhtvision-`cat version.txt`.src.tar.bz2 $SET_RPM_DIR/rpm/SOURCES/librhtv-`cat version.txt`.tar.bz2
 rpmbuild -bb redhat/librhtv-`cat version.txt`.spec
 mv redhat/.rpmmacros-backup~ ~/.rpmmacros
 
