935826061aa022f4eb8534b08f5845ee134d6e7d
set <>
2003-07-16 13:42:34 +0000

    * Modified: The RPM scripts to support an environment variable to indicate where to create the RPMs. I needed it when using S.F. Compiler Farm and Red Hat 7.3 which always failed when using a directory mounted with NFS.

--- tvision/redhat/create-dirs.sh
+++ tvision/redhat/create-dirs.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-mkdir ~/rpm
-mkdir ~/rpm/BUILD
-mkdir ~/rpm/RPMS
-mkdir ~/rpm/RPMS/i386
-mkdir ~/rpm/SOURCES
-mkdir ~/rpm/SPECS
-mkdir ~/rpm/SRPMS
+if [ "$SET_RPM_DIR" ]; then
+   SET_RPM_DIR="$SET_RPM_DIR";
+else
+   SET_RPM_DIR="$HOME";
+fi
+mkdir $SET_RPM_DIR/rpm
+mkdir $SET_RPM_DIR/rpm/BUILD
+mkdir $SET_RPM_DIR/rpm/RPMS
+mkdir $SET_RPM_DIR/rpm/RPMS/i386
+mkdir $SET_RPM_DIR/rpm/SOURCES
+mkdir $SET_RPM_DIR/rpm/SPECS
+mkdir $SET_RPM_DIR/rpm/SRPMS
