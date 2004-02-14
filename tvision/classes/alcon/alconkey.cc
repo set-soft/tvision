9282ae4b38820034ea38a77e19e1efd7dd7c6dfb
gradha <>
2004-02-14 19:14:37 +0000

    Basic key functions.

---
+++ tvision/classes/alcon/alconkey.cc
@@ -0,0 +1,41 @@
+// -*- mode:C++; tab-width: 4 -*-
+#include <tv/configtv.h>
+
+#define Uses_TScreen
+#define Uses_TEvent
+#define Uses_TGKey
+#include <tv.h>
+
+#include <tv/alcon/key.h>
+#define Uses_AlCon_conio
+#include <tv/alcon/alcon.h>
+
+
+void TGKeyAlcon::Init()
+{
+	TGKey::kbhit = KbHit;
+	TGKey::clear = Clear;
+	TGKey::gkey = GKey;
+//	TGKey::getShiftState=GetShiftState;
+//	TGKey::fillTEvent = FillTEvent;
+}
+
+int TGKeyAlcon::KbHit()
+{
+	return AlCon_KbHit();
+}
+
+void TGKeyAlcon::Clear()
+{
+	AlCon_ClearKeyBuf();
+}
+
+ushort TGKeyAlcon::GKey()
+{
+	unsigned aSymbol;
+	uchar aScan;
+	unsigned aFlags;
+	
+	return AlCon_GetKey(&aSymbol, &aScan, &aFlags);
+}
+
