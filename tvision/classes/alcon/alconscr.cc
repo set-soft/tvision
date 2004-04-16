dcbc9b148b22b758eb041f0955e10c1a2e70fc12
gradha <>
2004-04-16 21:03:19 +0000

    Ok, creating a screenBuffer allows Allegro to draw things. Oh, but they are skewed. Somewhere I'm dividing by 2...

--- tvision/classes/alcon/alconscr.cc
+++ tvision/classes/alcon/alconscr.cc
@@ -29,7 +29,7 @@ TScreenAlcon::TScreenAlcon()
 {
     printf("TScreenAlcon(): start\n");
     // Create screen.
-    AlCon_Init(80, 25);
+    AlCon_Init(screenWidth, screenHeight);
     
     // Set background to black and foreground to light gray.
     textattr(LIGHTGRAY | (BLACK<<4));
@@ -37,7 +37,7 @@ TScreenAlcon::TScreenAlcon()
     // Jump to top left (1, 1).
     gotoxy(1, 1);
 
-	cputs("TScreenAlcon was just initialised\n");
+    cputs("TScreenAlcon was just initialised\n a");
 
     TDisplayAlcon::Init();
 
@@ -51,7 +51,16 @@ TScreenAlcon::TScreenAlcon()
     TScreen::getFontGeometry=GetFontGeometry;
 //    TScreen::setFont_p=SetFont;
 //    TScreen::restoreFonts=RestoreFonts;
-	initialized=1;
+    initialized=1;
+
+    // Create memory buffer for screen. We want a buffer (even though
+    // AlCon is buffered) because otherwise TVision will use simple
+    // memcpy calls to "paint" the screen. We don't want this, because
+    // Allegro has to be told when to update the screen. So it's a
+    // buffer on top of Allegro's buffer so that Allegro get's called.
+    // Wicked. But works.
+    screenBuffer = new uint16[screenWidth * screenHeight];
+    
     printf("TScreenAlcon(): end\n");
 }
 
@@ -59,6 +68,8 @@ TScreenAlcon::TScreenAlcon()
 TScreenAlcon::~TScreenAlcon()
 {
     printf("~TScreenAlcon\n");
+    delete[] screenBuffer;
+    
     // Deinitialize all.
     AlCon_Exit();
 }
@@ -67,7 +78,7 @@ TScreenAlcon::~TScreenAlcon()
 void TScreenAlcon::clearScreen()
 {
     printf("TScreenAlcon::clearScreen()\n");
-	TDisplay::clearScreen(AlCon_ScreenCols(), AlCon_ScreenCols());
+    TDisplay::clearScreen(AlCon_ScreenCols(), AlCon_ScreenCols());
 }
 
 
@@ -75,7 +86,7 @@ ushort TScreenAlcon::getCharacter(unsigned offset)
 {
     printf("TScreenAlcon::getCharacter %d\n", offset);
     ushort buffer;
-	AlCon_GetScrChars(offset, &buffer, 1);
+    AlCon_GetScrChars(offset, &buffer, 1);
     return buffer;
 }
 
@@ -83,30 +94,30 @@ ushort TScreenAlcon::getCharacter(unsigned offset)
 void TScreenAlcon::getCharacters(unsigned offset, ushort *buffer, unsigned count)
 {
     printf("TScreenAlcon::getCharacters offset %d, count %d\n", offset, count);
-	AlCon_GetScrChars(offset, buffer, count);
+    AlCon_GetScrChars(offset, buffer, count);
 }
 
 
 void TScreenAlcon::setCharacter(unsigned offset, uint32 value)
 {
     printf("TScreenAlcon::setCharacter offset %d value %d\n", offset, value);
-	AlCon_PutChar(offset, value);
+    AlCon_PutChar(offset, value);
 }
 
 
 void TScreenAlcon::setCharacters(unsigned offset, ushort *values, unsigned count)
 {
     printf("TScreenAlcon::setCharacters offset %d, count %d\n", offset, count);
-	AlCon_PutBuf(offset, values, count);
+    AlCon_PutBuf(offset, values, count);
 }
 
 
 int TScreenAlcon::GetFontGeometry(unsigned &w, unsigned &h)
 {
     printf("TScreenAlcon::GetFontGeometry\n");
-	w = 8;
+    w = 8;
     h = 16;
-	return 1;
+    return 1;
 }
 
 
