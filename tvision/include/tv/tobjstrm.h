3bcf1816abc6f3ae8cf2f6a7a77ed8ed2acea9f6
Salvador E. Tropea <salvador@inti.gob.ar>
2004-10-05 21:40:26 +0000

    * Fixed(?!): [gcc 3.4.1] For some, unknown, reason the mechanism to "pull" stream dependencies fails for Mandrake 10.0 (gcc 3.4.1 + glibc 2.3.3 + binutils 2.14.90.0.7 20031029). So now I'm changing to the mechanism we used for BC++ when 3.4.1 is detected.

--- tvision/include/tv/tobjstrm.h
+++ tvision/include/tv/tobjstrm.h
@@ -13,19 +13,40 @@ Modified by Robert H”hne to be used for RHIDE.
 
 typedef unsigned P_id_type;
 
-#if !defined( __fLink_def )
-#define __fLink_def
+// SET: Here we implement a trick to pull the needed TStreamableClass-es.
+// We have two approaches:
+// 1) We create a structure with a pointer to itself and another to the
+//    object to register the stream for the class.
+// 2) Same but we use a constructor in the structure.
+// The first methode was tested with gcc 2.6.3 to 3.4.1. It seems to fail
+// for gcc 3.4.1, but we know it works for most versions of gcc.
+// The second was specifically created to BC++ compiler.
+// The BC++ approach should work for any C++ compiler, but I can't be sure.
+// For this reason I'm keeping both and switching to the second approach
+// only for gcc>=3.4.
+// After the v2.0.3 release we could try to test if (2) is supported by all
+// targets.
+
+#undef TV_Use_Constructor_For_fLink_def
+#if defined(TVComp_BCPP) || (__GNUC__>=3 && __GNUC_MINOR__>=4)
+ #define TV_Use_Constructor_For_fLink_def 1
+#else
+ #define TV_Use_Constructor_For_fLink_def 0
+#endif
+
+#if !defined(TV__fLink_def)
+#define TV__fLink_def
 struct fLink
 {
     fLink *f;
     class TStreamableClass *t;
-    #ifdef TVComp_BCPP
+    #if TV_Use_Constructor_For_fLink_def
     fLink(fLink* af, class TStreamableClass *at) : f(af), t(at) {}
     #endif
 };
 #endif
 
-#ifdef TVComp_BCPP
+#if TV_Use_Constructor_For_fLink_def
 #define __link( s )             \
   extern TStreamableClass s;    \
   static fLink force ## s       \
