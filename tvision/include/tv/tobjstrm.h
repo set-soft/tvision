a0addf06c92c1c9ce49c52983b37c5db554a389d
set <>
2005-07-08 20:23:28 +0000

    * Added: [gcc] Code to solve problems related to gcc 3.4.x and 4.0.0. The solution was provided by Andris, but IMHO that's a workaround for gcc bugs instead of a solution.

--- tvision/include/tv/tobjstrm.h
+++ tvision/include/tv/tobjstrm.h
@@ -21,15 +21,22 @@ typedef unsigned P_id_type;
 // The first methode was tested with gcc 2.6.3 to 3.4.1. It seems to fail
 // for gcc 3.4.1, but we know it works for most versions of gcc.
 // The second was specifically created to BC++ compiler.
-// The BC++ approach should work for any C++ compiler, but I can't be sure.
+// The BC++ approach should work for any C++ compiler, but fails for gcc 3.3.5.
 // For this reason I'm keeping both and switching to the second approach
 // only for gcc>=3.4.
-// After the v2.0.3 release we could try to test if (2) is supported by all
-// targets.
+// GCC 3.4.x and 4.0.0 introduces another problem: statics are optimized out.
+// Even when they have side-effects.
+// Another option for gcc (suggested by Andris):
+// #define __link( s ) __asm__(".global  _"#s);
 
 #undef TV_Use_Constructor_For_fLink_def
-#if defined(TVComp_BCPP) || (__GNUC__>=3 && __GNUC_MINOR__>=4)
+#undef TV_Use_Namespace_instead_of_Static
+#if defined(TVComp_BCPP)
  #define TV_Use_Constructor_For_fLink_def 1
+ #define TV_Use_Namespace_instead_of_Static 0
+#elif __GNUC__>=3 && (__GNUC__>=4 || __GNUC_MINOR__>=4)
+ #define TV_Use_Constructor_For_fLink_def 1
+ #define TV_Use_Namespace_instead_of_Static 1
 #else
  #define TV_Use_Constructor_For_fLink_def 0
 #endif
@@ -47,15 +54,34 @@ struct fLink
 #endif
 
 #if TV_Use_Constructor_For_fLink_def
-#define __link( s )             \
-  extern TStreamableClass s;    \
-  static fLink force ## s       \
-    ( (fLink *)&force ## s, (TStreamableClass *)&s );
+ // This approach fails for gcc 3.3.5 so we can't use it for all gcc versions.
+ #if TV_Use_Namespace_instead_of_Static
+  // Ok for gcc 3.4.4 and 4.0.0
+  // Suggested by Andris Pavenis he said: Anonymous namespace is very similar to static
+  // (it's a separate for each compiled source). So we are not going to have name
+  // collisions.
+  #define __link( s )             \
+    extern TStreamableClass s;    \
+    namespace {                   \
+    fLink force ## s              \
+      ( (fLink *)&force ## s, (TStreamableClass *)&s ); \
+    }
+ #else
+  // Ok for BC++ 5.5/6
+  // Fails with gcc 3.4+ because the static code gets "optimized" That's a bug from my
+  // point of view because the static code have external side-effects (calls a constructor)
+  #define __link( s )             \
+    extern TStreamableClass s;    \
+    static fLink force ## s       \
+      ( (fLink *)&force ## s, (TStreamableClass *)&s );
+ #endif
 #else
-#define __link( s )             \
-  extern TStreamableClass s;    \
-  static fLink force ## s =     \
-    { (fLink *)&force ## s, (TStreamableClass *)&s };
+ // Ok for gcc 2.7.2, 2.8.1, 2.95.3/4, 3.3.5 and all MSVC versions we tried.
+ // Fails for gcc 3.4+ read above.
+ #define __link( s )             \
+   extern TStreamableClass s;    \
+   static fLink force ## s =     \
+     { (fLink *)&force ## s, (TStreamableClass *)&s };
 #endif
 
 #if defined( Uses_TStreamable )
