74329a26eafaf82cd5682374643799e5dc8a46e1
Salvador E. Tropea <salvador@inti.gob.ar>
2005-07-29 13:21:26 +0000

    * Added: [gcc] Workaround for a gcc 2.95.2 bug. It looks like inline class members that have an extra ; at the end of the function body can confuse gcc (only in particular cases). Reported by Thiago.

--- tvision/include/tv/nointl.h
+++ tvision/include/tv/nointl.h
@@ -15,21 +15,21 @@ extern CLY_EXPORT char *newStr( const char * );
 class CLY_EXPORT TVIntl
 {
 public:
- TVIntl() {};
- static const char *textDomain(const char *) { return NULL; };
- static const char *bindTextDomain(const char *, const char *) { return NULL; };
- static int         autoInit(const char *package, const char *localeDir=NULL) { return 1; };
- static const char *getText(const char *msgid) { return msgid; };
- static void        setCatalogEncoding(int ) { };
- static void        enableTranslations() { };
- static void        disableTranslations() { };
+ TVIntl() {}
+ static const char *textDomain(const char *) { return NULL; }
+ static const char *bindTextDomain(const char *, const char *) { return NULL; }
+ static int         autoInit(const char *package, const char *localeDir=NULL) { return 1; }
+ static const char *getText(const char *msgid) { return msgid; }
+ static void        setCatalogEncoding(int ) {}
+ static void        enableTranslations() {}
+ static void        disableTranslations() {}
  static char       *getTextNew(const char *msgid, Boolean onlyIntl=False)
                     { return onlyIntl ? NULL : newStr(msgid); }
  static const char *getText(const char *msgid, stTVIntl *&)
-                    { return msgid; };
- static void        freeSt(stTVIntl *&) { };
- static stTVIntl   *emptySt() { return NULL; };
- static stTVIntl   *dontTranslateSt() { return NULL; };
+                    { return msgid; }
+ static void        freeSt(stTVIntl *&) {}
+ static stTVIntl   *emptySt() { return NULL; }
+ static stTVIntl   *dontTranslateSt() { return NULL; }
  static int         snprintf(char *dest, size_t sz, const char *fmt, ...);
  #ifdef Uses_intl_fprintf
  static int         fprintf(FILE *f, const char *fmt, ...);
