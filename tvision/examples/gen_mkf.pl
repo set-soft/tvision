7afa59fdfc3981a28b43e4110789f87ae12c8205
Salvador E. Tropea <salvador@inti.gob.ar>
2012-03-19 14:09:09 +0000

    * Fixed: The first time we link the library (on clean systems) rhtv doesn't yet exist. It can produce link errors if we include the lib in the list.

--- tvision/examples/gen_mkf.pl
+++ tvision/examples/gen_mkf.pl
@@ -1,26 +1,30 @@
 #!/usr/bin/perl
+# To update all the mkf files use: perl gen_mkf.pl `find . -name "*.mkf"`
+# To create one: perl gen_mkf.pl ./DIR/FILE.mkf
 
 for (@ARGV)
    {
     $_=~/([^\/]*)\.mkf/;
     $make=$1;
-    $c=scalar(split(/\//));
+    $c=scalar(split(/\//))-1;
     $d='';
     while ($c--) { $d.='../'; }
     print "$_ $1 $d\n";
     open(FIL,">$_");
     print FIL "#!/usr/bin/make
 #
-# Copyright (c) 2003-2004 by Salvador E. Tropea.
+# Copyright (c) 2003-2012 by Salvador E. Tropea.
 # Covered by the GPL license.
 #
 vpath_src=
 vpath_obj=.
 INCLUDE_DIRS=".$d."include
 COMMON_IMK=".$d."makes/common.imk
+LIBS=rhtv
 include \$(COMMON_IMK)
 include $1\$(ExIMK)
 ";
     close(FIL);
    }
+
 
