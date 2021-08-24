ac663a0c130d9e8f6d9e7ce167662e532f7879f0
Salvador E. Tropea <salvador@inti.gob.ar>
2021-08-24 11:40:10 -0300

    Fixed missing strcpy declaration in i18n example. Fixes #2

--- tvision/examples/i18n/test.cc
+++ tvision/examples/i18n/test.cc
@@ -14,6 +14,7 @@ different.
 ***************************************************************************/
 
 #define Uses_stdlib
+#define Uses_string
 #define Uses_TApplication
 #define Uses_TKeys
 #define Uses_TRect
